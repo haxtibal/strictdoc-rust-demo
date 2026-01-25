2fdd890f14bface6fe3db96e18df9e7b713f6b51
Tobias Deiminger <tobias.deiminger@linutronix.de>
2026-01-25 17:33:36 +0100

    Enhance requirements and test samples

---
+++ src/source_nodes.rs
@@ -0,0 +1,10 @@
+/**
+  * UID: AUTO-NODE-1
+  * TITLE: A requirement extracted from Rust source code
+  * STATEMENT:
+  *   StrictDoc's source node feature extracts document nodes from key:value pairs in source code comments,
+  *   and automatically links them with the commented function. The Rust reader supports this feature.
+  *
+  * @relation(RUST-SOURCE-NODES)
+  */
+fn foo() {}
